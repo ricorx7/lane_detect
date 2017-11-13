63ff282fa75a50a0b7b6a13ff34d26b6872b54ba
Rico <ricorx7@gmail.com>
2017-11-12 22:17:33 -0800

    Updated the parameters to get a better result Added new test images

--- image_pipeline.py
+++ image_pipeline.py
@@ -6,6 +6,52 @@ from line import Line
 solid_lines = True
 temporal_smoothing = True
 
+def pipeline(image_path):
+    image = cv2.imread(image_path)
+
+    # Set image sizes
+    img_h, img_w = image.shape[0], image.shape[1]
+
+    # Crop image to remove background
+    height, width, channels = image.shape
+    #print(image.shape)
+    cropTop = int(round(height * 0.45))
+    cropBottom = int(round(height * 1.0))
+    cropped = image[cropTop:cropBottom, :, :]
+
+    # convert to grayscale
+    img_gray = cv2.cvtColor(cropped, cv2.COLOR_BGR2GRAY)
+
+    # perform gaussian blur
+    #17,17 deafualt kernel
+    img_blur = cv2.GaussianBlur(img_gray, (5, 5), 0)
+
+    # perform edge detection
+    img_edge = cv2.Canny(img_blur, threshold1=50, threshold2=80)
+
+    # perform hough transform
+    detected_lines = cv2.HoughLinesP(img_edge,
+                                     2,
+                                     np.pi / 180,
+                                     1,
+                                     np.array([]),
+                                     minLineLength=7,      # default 15
+                                     maxLineGap=5)          # default 5
+
+    # convert (x1, y1, x2, y2) tuples into Lines
+    detected_lines = [Line(l[0][0], l[0][1], l[0][2], l[0][3]) for l in detected_lines]
+
+    # draw lanes found
+    # prepare empty mask on which lines are drawn
+    #line_img = np.zeros(shape=(img_h, img_w))
+    line_img = image
+    for lane in detected_lines:
+        if 0.1 <= np.abs(lane.slope) <= 4:
+            lane.draw(line_img, offset_y=cropTop)       # Add in cropped location
+
+    # Return the image with lines drawn on it
+    return line_img
+
 def compute_lane_from_candidates(line_candidates, img_shape):
     """
     Compute lines that approximate the position of both road lanes.
@@ -75,102 +121,110 @@ def region_of_interest(img, vertices):
 
     return masked_image, mask
 
-# Read Image
-image = cv2.imread('test_image_2.jpg')
-img_h, img_w = image.shape[0], image.shape[1]
+def debug_pipeline():
+    # Read Image
+    image = cv2.imread('test_image_2.jpg')
 
-# Crop image to remove background
-height, width, channels = image.shape
-print(image.shape)
-cropTop = int(round(height * 0.45))
-cropBottom = int(round(height * 1.0))
-cropped = image[cropTop:cropBottom, :, :]
-cv2.imwrite('1_cropped.jpg', cropped)
-print('Cropped')
-#cropped = image
+    img_h, img_w = image.shape[0], image.shape[1]
+
+    # Crop image to remove background
+    height, width, channels = image.shape
+    print(image.shape)
+    cropTop = int(round(height * 0.45))
+    cropBottom = int(round(height * 1.0))
+    cropped = image[cropTop:cropBottom, :, :]
+    cv2.imwrite('1_cropped.jpg', cropped)
+    print('Cropped')
+    #cropped = image
 
 
-# convert to grayscale
-img_gray = cv2.cvtColor(cropped, cv2.COLOR_BGR2GRAY)
-cv2.imwrite("2_grayscale.jpg", img_gray)
-print("Grayscale")
+    # convert to grayscale
+    img_gray = cv2.cvtColor(cropped, cv2.COLOR_BGR2GRAY)
+    cv2.imwrite("2_grayscale.jpg", img_gray)
+    print("Grayscale")
 
-# perform gaussian blur
-#17,17 deafualt kernel
-img_blur = cv2.GaussianBlur(img_gray, (5, 5), 0)
-cv2.imwrite("3_blur.jpg", img_blur)
-print("Blur")
+    # perform gaussian blur
+    #17,17 deafualt kernel
+    img_blur = cv2.GaussianBlur(img_gray, (5, 5), 0)
+    cv2.imwrite("3_blur.jpg", img_blur)
+    print("Blur")
 
-# perform edge detection
-img_edge = cv2.Canny(img_blur, threshold1=50, threshold2=80)
-cv2.imwrite("4_canny.jpg", img_edge)
-print("Canny")
+    # perform edge detection
+    img_edge = cv2.Canny(img_blur, threshold1=50, threshold2=80)
+    cv2.imwrite("4_canny.jpg", img_edge)
+    print("Canny")
 
-# perform hough transform
-detected_lines = cv2.HoughLinesP(img_edge,
-                                 2,
-                                 np.pi / 180,
-                                 1,
-                                 np.array([]),
-                                 minLineLength=7,      # default 15
-                                 maxLineGap=5)          # default 5
+    # perform hough transform
+    detected_lines = cv2.HoughLinesP(img_edge,
+                                     2,
+                                     np.pi / 180,
+                                     1,
+                                     np.array([]),
+                                     minLineLength=7,      # default 15
+                                     maxLineGap=5)          # default 5
 
-print("Lines")
-print(detected_lines)
+    print("Lines")
+    print(detected_lines)
 
-# convert (x1, y1, x2, y2) tuples into Lines
-detected_lines = [Line(l[0][0], l[0][1], l[0][2], l[0][3]) for l in detected_lines]
-print("Detected Lines: ", len(detected_lines))
+    # convert (x1, y1, x2, y2) tuples into Lines
+    detected_lines = [Line(l[0][0], l[0][1], l[0][2], l[0][3]) for l in detected_lines]
+    print("Detected Lines: ", len(detected_lines))
 
-# if 'solid_lines' infer the two lane lines
-#if solid_lines:
-#    candidate_lines = []
-#    for line in detected_lines:
-#        # consider only lines with slope between 30 and 60 degrees
-#        if 0.5 <= np.abs(line.slope) <= 2:
-#            candidate_lines.append(line)
-#            print("candidate line found")
-#    # interpolate lines candidates to find both lanes
-#    lane_lines = compute_lane_from_candidates(candidate_lines, img_gray.shape)
-#else:
-#    # if not solid_lines, just return the hough transform output
-#    lane_lines = detected_lines
+    # if 'solid_lines' infer the two lane lines
+    #if solid_lines:
+    #    candidate_lines = []
+    #    for line in detected_lines:
+    #        # consider only lines with slope between 30 and 60 degrees
+    #        if 0.5 <= np.abs(line.slope) <= 2:
+    #            candidate_lines.append(line)
+    #            print("candidate line found")
+    #    # interpolate lines candidates to find both lanes
+    #    lane_lines = compute_lane_from_candidates(candidate_lines, img_gray.shape)
+    #else:
+    #    # if not solid_lines, just return the hough transform output
+    #    lane_lines = detected_lines
 
-#lane_lines = []
-#for line in detected_lines:
-#    print("Line Slope: ", line.slope)
-# consider only lines with slope between 30 and 60 degrees
-#    if 0.2 <= np.abs(line.slope) <= 2:
-#        lane_lines.append(line)
-#print("Sort out the lines")
-#print("Lane Line Count: ", len(lane_lines))
-lane_lines = detected_lines
+    #sorted_lines = []
+    #for line in detected_lines:
+    #    print("Line Slope: ", line.slope)
+    # consider only lines with slope between 30 and 60 degrees
+    #    if 0.1 <= np.abs(line.slope) <= 4:      # 0.2 to 2
+    #        sorted_lines.append(line)
+    #print("Sort out the lines")
+    #print("Lane Line Count: ", len(lane_lines))
+    #lane_lines = compute_lane_from_candidates(sorted_lines, img_gray.shape)
+    lane_lines = detected_lines
 
-# draw lanes found
-# prepare empty mask on which lines are drawn
-#line_img = np.zeros(shape=(img_h, img_w))
-line_img = image
-for lane in lane_lines:
-    #print("Selected LIne: X1", lane.x1)
-    #print("Selected LIne: Y1", lane.y1)
-    #print("Selected LIne: X2", lane.x2)
-    #print("Selected LIne: Y2", lane.y2)
-    lane.draw(line_img, offset_y=cropTop)       # Add in cropped location
-cv2.imwrite("5_lines.jpg", line_img)
-print("Lines drawn")
+    # draw lanes found
+    # prepare empty mask on which lines are drawn
+    #line_img = np.zeros(shape=(img_h, img_w))
+    line_img = image
+    for lane in lane_lines:
+        print("Slope: ", lane.slope)
+        if 0.1 <= np.abs(lane.slope) <= 4:
+            #print("Selected LIne: X1", lane.x1)
+            #print("Selected LIne: Y1", lane.y1)
+            #print("Selected LIne: X2", lane.x2)
+            #print("Selected LIne: Y2", lane.y2)
+            lane.draw(line_img, offset_y=cropTop)       # Add in cropped location
+    cv2.imwrite("5_lines.jpg", line_img)
+    print("Lines drawn")
 
-"""
-# keep only region of interest by masking
-vertices = np.array([[(50, img_h),
-                      (450, 310),
-                      (490, 310),
-                      (img_w - 50, img_h)]],
-                    dtype=np.int32)
-img_masked, _ = region_of_interest(line_img, vertices)
-print("image mask")
-cv2.imwrite("img_mask.jpg", img_masked)
+    """
+    # keep only region of interest by masking
+    vertices = np.array([[(50, img_h),
+                          (450, 310),
+                          (490, 310),
+                          (img_w - 50, img_h)]],
+                        dtype=np.int32)
+    img_masked, _ = region_of_interest(line_img, vertices)
+    print("image mask")
+    cv2.imwrite("img_mask.jpg", img_masked)
+    
+    img_blend = weighted_img(img_masked, image, α=0.8, β=1., λ=0.)
+    print("Final image drawn")
+    cv2.imwrite("final.jpg", img_blend)
+    """
 
-img_blend = weighted_img(img_masked, image, α=0.8, β=1., λ=0.)
-print("Final image drawn")
-cv2.imwrite("final.jpg", img_blend)
-"""
+if __name__ == "__main__":
+    debug_pipeline()
--- line.py
+++ line.py
@@ -28,7 +28,7 @@ class Line:
         return np.array([self.x1, self.y1, self.x2, self.y2])
 
     def set_coords(self, x1, y1, x2, y2):
-        self.x1 = x1
+        self.x1 = x1s
         self.y1 = y1
         self.x2 = x2
         self.y2 = y2
