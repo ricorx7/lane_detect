7141ced4ba35b4b9b0f77fb5304b6fc0e02e19fe
Rico <ricorx7@gmail.com>
2017-11-13 23:12:53 -0800

    Working on averaging the lines

--- image_pipeline.py
+++ image_pipeline.py
@@ -120,10 +120,98 @@ def region_of_interest(img, vertices):
     masked_image = cv2.bitwise_and(img, mask)
 
     return masked_image, mask
+# by a given line mx + b
+# returns tuple (x1, y1, x2, y2)
+# where f(x1) = y1, f(x2) = y2
+def compute_line(m, b, x1, x2):
+    #return int(x1), int(m*x1 + b), int(x2), int(m*x2 + b)
+    return Line(int(x1), int(m*x1 + b), int(x2), int(m*x2 + b))
+
+
+def draw_lines(img, lines, color=[255, 0, 0], thickness=2):
+    """
+    NOTE: this is the function you might want to use as a starting point once you want to
+    average/extrapolate the line segments you detect to map out the full
+    extent of the lane (going from the result shown in raw-lines-example.mp4
+    to that shown in P1_example.mp4).
+
+    Think about things like separating line segments by their
+    slope ((y2-y1)/(x2-x1)) to decide which segments are part of the left
+    line vs. the right line.  Then, you can average the position of each of
+    the lines and extrapolate to the top and bottom of the lane.
+
+    This function draws `lines` with `color` and `thickness`.
+    Lines are drawn on the image inplace (mutates the image).
+    If you want to make the lines semi-transparent, think about combining
+    this function with the weighted_img() function below
+    """
+    if lines is None or len(lines) == 0:
+        return img
+
+    left_line_m_sum = 0
+    left_line_b_sum = 0
+    left_min_b = 0
+    number_of_left_lines = 0
+
+    right_line_m_sum = 0
+    right_line_b_sum = 0
+    right_min_b = 0
+    number_of_right_lines = 0
+    for line in lines:
+        #for x1, y1, x2, y2 in line:
+        if line.x1 == line.x2:
+            # skip if the line is perpendicular to screen
+            continue
+        m = ((line.y2 - line.y1) / (line.x2 - line.x1))
+        b = line.y1 - m * line.x1
+
+        # Filter outliers
+        if abs(m) <= 0.0:
+            print("Bad Slope: ", m)
+            continue
+
+        if m > 0:
+            left_line_m_sum += m
+            left_line_b_sum += b
+            number_of_left_lines += 1
+            left_min_b = min(left_min_b, line.y1, line.y2)
+            print("Left Slope: ", m)
+        else:
+            right_line_m_sum += m
+            right_line_b_sum += b
+            number_of_right_lines += 1
+            right_min_b = min(right_min_b, line.y1, line.y2)
+            print("Right Slope: " , m)
+
+    averaged_lines = []
+    width = img.shape[1]
+
+    if number_of_left_lines > 0:
+        left_m = left_line_m_sum / float(number_of_left_lines)
+        left_b = left_line_b_sum / float(number_of_left_lines)
+        #left_b = left_min_b
+
+        left_line = [compute_line(left_m, left_b, width, width * 0.55)]
+        averaged_lines.append(left_line)
+
+    if number_of_right_lines > 0:
+        right_m = right_line_m_sum / float(number_of_right_lines)
+        right_b = right_line_b_sum / float(number_of_right_lines)
+        #right_b = right_min_b
+
+        right_line = [compute_line(right_m, right_b, -width * 0.55, width * 0.45)]
+        averaged_lines.append(right_line)
+
+    #for averaged_line in averaged_lines:
+    #    for x1, y1, x2, y2 in averaged_line:
+    #        cv2.line(img, (x1, y1), (x2, y2), color, 15)
+
+    return averaged_lines
+
 
 def debug_pipeline():
     # Read Image
-    image = cv2.imread('test_image_2.jpg')
+    image = cv2.imread('test_image_5.jpg')
 
     img_h, img_w = image.shape[0], image.shape[1]
 
@@ -193,38 +281,42 @@ def debug_pipeline():
     #print("Sort out the lines")
     #print("Lane Line Count: ", len(lane_lines))
     #lane_lines = compute_lane_from_candidates(sorted_lines, img_gray.shape)
-    lane_lines = detected_lines
+    #lane_lines = detected_lines
+    lane_lines = draw_lines(image, detected_lines)
+
+    print("Lane Lines: ", lane_lines)
 
     # draw lanes found
     # prepare empty mask on which lines are drawn
-    #line_img = np.zeros(shape=(img_h, img_w))
-    line_img = image
+    line_img = np.zeros(shape=(img_h, img_w))
+    #line_img = image
     for lane in lane_lines:
-        print("Slope: ", lane.slope)
-        if 0.1 <= np.abs(lane.slope) <= 4:
+        print("Lane: ", lane[0].x1)
+        print("Slope: ", lane[0].slope)
+        #if 0.1 <= np.abs(lane.slope) <= 4:
             #print("Selected LIne: X1", lane.x1)
             #print("Selected LIne: Y1", lane.y1)
             #print("Selected LIne: X2", lane.x2)
             #print("Selected LIne: Y2", lane.y2)
-            lane.draw(line_img, offset_y=cropTop)       # Add in cropped location
+            #lane[0].draw(line_img, offset_y=cropTop)       # Add in cropped location
+        lane[0].draw(line_img, offset_y=0, offset_x=0)  # Add in cropped location
     cv2.imwrite("5_lines.jpg", line_img)
     print("Lines drawn")
 
-    """
     # keep only region of interest by masking
-    vertices = np.array([[(50, img_h),
-                          (450, 310),
-                          (490, 310),
-                          (img_w - 50, img_h)]],
-                        dtype=np.int32)
-    img_masked, _ = region_of_interest(line_img, vertices)
-    print("image mask")
-    cv2.imwrite("img_mask.jpg", img_masked)
+    #vertices = np.array([[(0, img_h * 0.85),
+    #                      (0, 0),
+    #                      (img_w, 0),
+    #                      (img_w, img_h * 0.85)]],
+    #                    dtype=np.int32)
+    #img_masked, _ = region_of_interest(line_img, vertices)
+    #print("image mask")
+    #cv2.imwrite("6_img_mask.jpg", img_masked)
     
-    img_blend = weighted_img(img_masked, image, α=0.8, β=1., λ=0.)
+    img_blend = weighted_img(line_img, image, α=0.8, β=1., λ=0.)
     print("Final image drawn")
-    cv2.imwrite("final.jpg", img_blend)
-    """
+    cv2.imwrite("7_final.jpg", img_blend)
+
 
 if __name__ == "__main__":
     debug_pipeline()
